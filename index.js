65c4e56c61220288a6df27d6e03c571f163f5267
stephen talley <stvlley.amzn@gmail.com>
2022-01-13 13:30:08 -0600

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Write your solution in this file!
+const employee = {
+    name:"stephen",
+    streetAddress: "11 Broadway",
+};
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    return {
+        ...employee,
+        [key]: value,
+    };
+}
+const newEmployee = updateEmployeeWithKeyAndValue(
+    employee,
+    "name",
+    "Sam"
+);
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+
+    return employee;
+}
+const updatedAddress = destructivelyUpdateEmployeeWithKeyAndValue(
+    employee["streetAddress"] = "12 Broadway"
+);
+function deleteFromEmployeeByKey(employee, key) {
+    console.log(key)
+    //#1 copy the original employee object
+    console.log(employee)
+    const nEmployee = {...employee}
+    console.log(nEmployee)
+    //delete the key
+    delete nEmployee.name
+    //return copied employee {}
+    return nEmployee;
+};    
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    
+    delete employee.name
+    //return copied employee {}
+    return employee;
+};
